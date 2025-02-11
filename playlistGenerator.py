459bd40efe610c2bc901d1f80f7ab8e6fc3215e4
galegoer <eric.m.galego@gmail.com>
2025-02-10 22:10:04 -0500

    added ability to create and add to playlist based on setlist url

--- playlistGenerator.py
+++ playlistGenerator.py
@@ -1,12 +1,18 @@
 import requests
 from bs4 import BeautifulSoup
+from dotenv import load_dotenv
+from youtube_client import YoutubeClient
 
 # TODO: type checking
 
 def retrieveHtml(url):
-    html = requests.get(url)
-    soup = BeautifulSoup(html.text, 'html.parser')
-    return soup
+    try:
+        html = requests.get(url)
+        soup = BeautifulSoup(html.text, 'html.parser')
+        return soup
+    except Exception:
+        print("Failed to retrieve HTML")
+        raise
 
 def getSongList(soup):
     songs = []
@@ -15,6 +21,22 @@ def getSongList(soup):
     return songs
 
 def getBandName(soup):
-    band = soup.find(class_="setlistHeadline").find('span')
+    band = soup.find(class_="setlistHeadline").find('span').text
     return band
 
+if __name__ == '__main__':
+    # TODO: change to reverse as well (Spotify to YT)
+    load_dotenv()
+
+    setlist_url = input("Enter the Setlist FM URL: ")
+
+    soup = retrieveHtml(setlist_url)
+    songs = getSongList(soup)
+    band = getBandName(soup)
+    print(f"Found {len(songs)} songs for {band}")
+    print(songs)
+    client = YoutubeClient()
+    playlist_id = client.createPlaylist(f"{band} Setlist") # TODO: add date to playlist title (band)
+    for song in songs:
+        song_id = client.getYoutubeSongId(song, band)
+        client.addSongToPlaylist(playlist_id, song_id)
